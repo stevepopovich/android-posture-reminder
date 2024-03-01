44bee09c91ea6568670bf171034237db10239cdb
Steven Popovich <stevepopovich8@gmail.com>
2024-02-29 22:34:47 -0600

    Setup basic datastore to try and have custom intervals

--- app/src/main/java/com/stevepopovich/posture_reminder/MainActivity.kt
+++ app/src/main/java/com/stevepopovich/posture_reminder/MainActivity.kt
@@ -5,22 +5,32 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
-import androidx.work.OneTimeWorkRequestBuilder
+import androidx.datastore.preferences.core.edit
 import androidx.work.WorkManager
-import androidx.work.WorkRequest
 import com.stevepopovich.posture_reminder.ui.theme.PostureReminderTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,9 +51,13 @@ class MainActivity : ComponentActivity() {
         }
 
         val reminderServiceIntent = Intent(this, ReminderForegroundService::class.java)
+
         setContent {
+            var minuteValue by remember { mutableIntStateOf(1) }
+            var secondValue by remember { mutableIntStateOf(0) }
+
+            val coroutineScope = rememberCoroutineScope()
             PostureReminderTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -55,13 +69,47 @@ class MainActivity : ComponentActivity() {
                     ) {
                         Button(onClick = {
                             this@MainActivity.startService(reminderServiceIntent)
+
+                            val totalSeconds = (minuteValue * 60) + secondValue
+                            val formatted = "${(totalSeconds / 60).toString().padStart(2, '0')}MM : ${(totalSeconds % 60).toString().padStart(2, '0')}SS"
+                            val toast = Toast.makeText(this@MainActivity, "Started with $formatted interval", Toast.LENGTH_SHORT)
+                            toast.show()
                         }) {
                             Text("Start Reminder Service")
                         }
                         Button(onClick = {
                             this@MainActivity.stopService(reminderServiceIntent)
+                            WorkManager.getInstance(applicationContext).cancelAllWork()
                         }) {
                             Text("Stop Reminder Service")
+                        }
+                        Row {
+                            val textFieldWidth = 80.dp
+                            TextField(
+                                modifier = Modifier.width(textFieldWidth),
+                                value = minuteValue.toString(),
+                                onValueChange = {
+                                    if (it.toIntOrNull() != null) {
+                                        minuteValue = it.toInt()
+                                        coroutineScope.launch {
+                                            saveMinutes(it.toInt())
+                                        }
+                                    }
+                                },
+                            )
+                            Text(text = ":")
+                            TextField(
+                                modifier = Modifier.width(textFieldWidth),
+                                value = secondValue.toString(),
+                                onValueChange = {
+                                    if (it.toIntOrNull() != null) {
+                                        secondValue = it.toInt()
+                                        coroutineScope.launch {
+                                            saveSeconds(it.toInt())
+                                        }
+                                    }
+                                },
+                            )
                         }
                     }
                 }
@@ -69,26 +117,15 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @Deprecated("Deprecated in Java")
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        // IDC if you granted permissions or not, I'm going to build the worker anyway
-        buildReminderWorker()
+    private suspend fun saveMinutes(minutes: Int) {
+       applicationContext.reminderDataStore.edit { settings ->
+           settings[MINUTES_KEY] = minutes
+       }
     }
 
-    private fun buildReminderWorker() {
-        val reminderWorkRequest: WorkRequest =
-            OneTimeWorkRequestBuilder<ReminderWorker>()
-                .addTag(REMINDER_WORKER_TAG)
-                .build()
-
-        WorkManager
-            .getInstance(this)
-            .enqueue(reminderWorkRequest)
+    private suspend fun saveSeconds(seconds: Int) {
+         applicationContext.reminderDataStore.edit { settings ->
+              settings[SECONDS_KEY] = seconds
+         }
     }
 }
---
+++ app/src/main/java/com/stevepopovich/posture_reminder/ReminderDataStore.kt
@@ -0,0 +1,12 @@
+package com.stevepopovich.posture_reminder
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.reminderDataStore: DataStore<Preferences> by preferencesDataStore(name = "reminder_settings")
+
+val MINUTES_KEY = intPreferencesKey("minutes_key")
+val SECONDS_KEY = intPreferencesKey("seconds_key")
