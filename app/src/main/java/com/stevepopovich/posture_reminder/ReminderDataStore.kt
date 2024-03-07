6a80426aa291ffa7ea95aae4b249d5987e7c6fdf
Steven Popovich <stevepopovich8@gmail.com>
2024-03-06 22:26:53 -0600

    Show time difference in pushes as an experiment

--- app/src/main/java/com/stevepopovich/posture_reminder/ReminderDataStore.kt
+++ app/src/main/java/com/stevepopovich/posture_reminder/ReminderDataStore.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.mutablePreferencesOf
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 
 val Context.reminderDataStore: DataStore<Preferences> by preferencesDataStore(name = "reminder_settings")
 
 val MINUTES_KEY = intPreferencesKey("minutes_key")
 val SECONDS_KEY = intPreferencesKey("seconds_key")
+val LAST_NOTIF_TIME = stringPreferencesKey("last_notif_time")
